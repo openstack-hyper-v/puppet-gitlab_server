09dfe349e7f4bd82b30aef931b5ba35ea5c2c11d
Tim Rogers <tiroger@microsoft.com>
2013-11-20 15:11:25 -0800

    correcting a misnamed file

---
+++ manifests/gitlab_add_users_to_groups.pp
@@ -0,0 +1,10 @@
+class gitlab_add_users_to_groups ( $gitlab_groups_users_list = hiera('gitlab::groups_users_list') ) {
+  $gitlab_groups_users_list.each { |$groupname, $users_list|
+    $users_list.each { |$user_email|
+      gitlab::group_user { "${groupname}_${user_email}":
+        user_email => $user_email,
+        groupname  => $groupname,
+      }
+    }
+  }
+}
